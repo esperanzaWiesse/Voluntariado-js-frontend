17607260df4a038d24d62d71f8459056cf058070
esperanzaWiesse <esperanzawiesse@gmail.com>
2025-12-01 23:11:05 -0500

    primero componentes en blanco

---
+++ src/app/shared/pagenot-found/pagenot-found.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagenot-found',
+  imports: [],
+  templateUrl: './pagenot-found.html',
+  styleUrl: './pagenot-found.css',
+})
+export class PagenotFound {
+
+}
